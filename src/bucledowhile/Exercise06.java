d387665bb6398989954f930590d53ca05662b2ca
christiangarcia-maker <christian.garcia@iesnervion.es>
2025-11-06 10:51:47 +0100

    Falta por comentar

--- src/bucledowhile/Exercise06.java
+++ src/bucledowhile/Exercise06.java
@@ -1,9 +1,41 @@
 package bucledowhile;
 
+import java.util.Scanner;
+
 public class Exercise06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Creación del scanner.
+		Scanner reader = new Scanner(System.in);
+		
+		// Declaramos las variables de las respuestas de los usuarios.
+		String respuesta1;
+		String respuesta2;
+		String opcion = "S";
+		
+		do {
+			// Preguntamos a los usuarios.
+			System.out.println("Jugador1, ¿piedra, papel o tijeras? ");
+			respuesta1 = reader.nextLine();
+
+			System.out.println("Jugador2, ¿piedra, papel o tijeras? ");
+			respuesta2 = reader.nextLine();
+			
+			if (respuesta1.equals(respuesta2)) {
+				System.out.println("Empate.");
+			} else if (respuesta1.equals("piedra") && respuesta2.equals("tijeras")
+					|| respuesta1.equals("tijeras") && respuesta2.equals("papel")
+					|| respuesta1.equals("papel") && respuesta2.equals("piedra")) {
+				System.out.println("¡Ha ganado el jugador 1!");
+			} else {
+				System.out.println("¡Ha ganado el jugador 2!");
+			}
+			System.out.println("Si quiere seguir jugando pulse S");
+			opcion = reader.nextLine();
+		} while (opcion.equals("S"));
+		
+		// Cierre del scanner.
+		reader.close();
 
 	}
 
--- src/bucledowhile/Exercise07.java
+++ src/bucledowhile/Exercise07.java
@@ -1,9 +1,73 @@
 package bucledowhile;
 
+import java.util.Scanner;
+
 public class Exercise07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+
+		// Variable para el primer número
+		int num1;
+
+		// Variable para el segundo número
+		int num2;
+
+		// Variable para la opción del menú
+		String respuesta;
+
+		// Variable para la suma
+		int suma;
+
+		// Variable para la resta
+		int resta;
+
+		// Variable para la multiplicación
+		int multiplicacion;
+
+		// Variable para la división
+		double division;
+
+		// Pedimos al usuario dos números
+		System.out.println("Introduzca dos números");
+		num1 = reader.nextInt();
+		num2 = reader.nextInt();
+
+		// Mostramos el menú
+		System.out.println("¿Qué operación desea realizar?");
+		System.out.println("A. SUMAR LOS NÚMEROS");
+		System.out.println("B. RESTAR LOS NÚMEROS");
+		System.out.println("C. MULTIPLICAR LOS NÚMEROS");
+		System.out.println("D. DIVIDIR LOS NÚMEROS");
+		System.out.println("E. SALIR");
+		respuesta = reader.next();
+
+		do {
+			if (respuesta.equals("A")) {
+				suma = num1 + num2;
+				System.out.println(num1 + "+" + num2 + "=" + suma);
+			} else if (respuesta.equals("B")) {
+				resta = num1 - num2;
+				System.out.println(num1 + "-" + num2 + "=" + resta);
+			} else if (respuesta.equals("C")) {
+				multiplicacion = num1 * num2;
+				System.out.println(num1 + "*" + num2 + "=" + multiplicacion);
+			} else if (respuesta.equals("D") && num2 != 0) {
+				division = num1 / num2;
+				System.out.println(num1 + "/" + num2 + "=" + division);
+			} else if (respuesta.equals("D") && num2 == 0) {
+				System.out.println("Error, no se puede dividir");
+			} else {
+				System.out.println(" ");
+			}
+
+			System.out.println("E. SALIR");
+			respuesta = reader.next();
+
+		} while (respuesta.equals("E"));
+
+		// Cerramos el scanner
+		reader.close();
 
 	}
 
